287bea484040eea738ffbabfefc11dbab8a34229
saikat111 <s8856010@gmail.com>
2021-12-10 21:03:58 +0600

    print the list in right order

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,65 +1,64 @@
-public class LinkedList {
+class LinkList {
     Node head;
     static class Node{
         int data;
         Node next;
+
         public Node(int d) {
-           data = d;
-           next = null;
+            data = d;
+            next = null;
         }
     }
-    void push(int new_data){
-        Node new_node = new Node(new_data);
+    void insert(int value){
+        Node new_node = new Node(value);
         new_node.next = head;
         head = new_node;
     }
-    void printList(){
+    void print(){
+        int count =0;
+        int ac = 0;
         Node tnode = head;
         while (tnode != null){
-            System.out.println(tnode.data);
+            count++;
             tnode = tnode.next;
         }
+        Node tnode2 = head;
+        int array[] = new int[count];
+        while (tnode2 != null){
+            array[ac] = tnode2.data;
+            ac++;
+            tnode2 = tnode2.next;
+        }
+        for (int i = array.length-1; i >= 0 ; i--){
+            System.out.println(array[i]);
+        }
     }
-    void deleteNode(int key){
-        Node temp = head , prev = null;
-        if(temp != null && temp.data == key){
-            head = temp.next;
-            return;
+    void printFromStart(){
+        Node p = head;
+        while (p != null){
+            System.out.println(p.data);
+            p = p.next;
         }
-        while (temp != null && temp.data !=key){
-            prev = temp;
-            temp = temp.next;
-        }
-        if(temp == null){
-            return;
-        }
-        prev.next = temp.next;
     }
-    public Boolean search(Node head , int x){
+    Boolean search(Node head , int key){
         Node current = head;
         while (current != null){
-            if(current.data == x)
+            if(current.data == key)
                 return true;
-                current = current.next;
+            current = current.next;
+
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        LinkedList llist = new LinkedList();
-        llist.push(2);
-        llist.push(7);
-        llist.push(3);
-        llist.printList();
-        System.out.println("delete");
-        llist.deleteNode(2);
-        llist.printList();
-        if(llist.search(llist.head,3)){
-            System.out.println("yes");
-        }
-        else {
-            System.out.println("no");
-        }
-
+    public static void main(String args[]){
+        LinkList linkList = new LinkList();
+        linkList.insert(22);
+        linkList.insert(25);
+        linkList.insert(222);
+        linkList.insert(2222);
+        linkList.print();
+        System.out.println("From start");
+        linkList.printFromStart();
+        System.out.println(linkList.search(linkList.head, 22));
     }
 }
